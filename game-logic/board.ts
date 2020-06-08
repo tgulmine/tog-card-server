7e406d4fe660d38463519ac1aee88a01fe742db6
tgulmine <tgulmine@gmail.com>
2020-06-07 22:27:05 -0300

    begin pick phase

--- game-logic/board.ts
+++ game-logic/board.ts
@@ -13,9 +13,12 @@ export class Board {
     start(playerOneName: string, playerTwoName: string) {
         this.players.push(new Player(playerOneName));
         this.players.push(new Player(playerTwoName));
-        let deck = new FullRegularDeck();
-        deck.createDeck();
-        deck.shuffleDeck();    
+        let d = new FullRegularDeck();
+        d.createDeck();
+        d.shuffleDeck();    
         //picking phase
+        //split deck
+        this.players[0].playerCards = d.deck.slice(0,14);
+        this.players[1].playerCards = d.deck.slice(15,29);
     }
 }
